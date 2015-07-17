1371c76cb87d9b14b7dd8a9df35c29458b82bb8b
Sean Hargadon <SHARGADON@bloomberg.net>
2015-07-17 05:04:29 -0400

    refactored code

--- groups/csa/csamisc/csamisc_emptystringinitialisation.cpp
+++ groups/csa/csamisc/csamisc_emptystringinitialisation.cpp
@@ -1,8 +1,8 @@
 #include <clang/ASTMatchers/ASTMatchers.h>
 #include <clang/ASTMatchers/ASTMatchFinder.h>
 #include <clang/Lex/Lexer.h>
-
 #include <clang/AST/Stmt.h>
+
 #include <csabase_analyser.h>
 #include <csabase_debug.h>
 #include <csabase_diagnostic_builder.h>
@@ -10,18 +10,18 @@
 #include <csabase_report.h>
 #include <csabase_util.h>
 #include <csabase_visitor.h>
-#include <set>
 
+#include <set>
 #include <iostream>
 
 using namespace csabase;
 using namespace clang;
 using namespace clang::ast_matchers;
 
-static std::string const check_name("emptystringinit");
+static std::string const check_name("string-initialisation");
 
 namespace {
-	
+    
 struct data {
 
 };
@@ -37,15 +37,18 @@ struct report : Report<data>
 
 
 //matches string initialization as a variable or in an initialization list
-internal::DynTypedMatcher dyn_matchEmptyStringInit()
+static const internal::DynTypedMatcher& dyn_matchEmptyStringInit()
 {
-	
-    internal::DynTypedMatcher matcher = findAll( decl( eachOf( 
-													varDecl( hasInitializer( 
-												    	hasDescendant ( stringLiteral().bind("string") ) 
-											    		)).bind("varDecl"),
-													constructorDecl( forEachConstructorInitializer( ctorInitializer(withInitializer(
-													hasDescendant( stringLiteral().bind("string")))).bind("varInit"))) ))); 
+    
+    static const internal::DynTypedMatcher& matcher = findAll( decl( eachOf( 
+                                                               varDecl( hasInitializer( 
+                                                                   hasDescendant ( stringLiteral().bind("string")) 
+                                                                   )).bind("varDecl"),
+                                                               constructorDecl( 
+                                                                   forEachConstructorInitializer( 
+                                                                       ctorInitializer( withInitializer(
+                                                                           hasDescendant( stringLiteral().bind("string")) 
+                                                                           )).bind("varInit")))))); 
     return matcher; 
 }
 
@@ -62,68 +65,49 @@ void report::operator()()
 void report::stringInit(BoundNodes const & nodes)
 {
     clang::CXXCtorInitializer const* varInit = nodes.getNodeAs<clang::CXXCtorInitializer>("varInit");
-	
-	std::string tag = "SI01";
-	std::string message = "strings should not be initialized with an empty string";
+    clang::VarDecl const * varDecl = nodes.getNodeAs<clang::VarDecl>("varDecl"); 
+    clang::StringLiteral const * str = nodes.getNodeAs<clang::StringLiteral>("string"); 
+    
+    //no null check needed as string should always be matched
+    if (str->getLength() != 0) return;
 
-
-	//if varibable declaration is matched 
-	if (varInit == nullptr) {
-		clang::VarDecl const * varDecl = nodes.getNodeAs<clang::VarDecl>("varDecl");  
-		std::string varType = varDecl->getInit()->getType().getAsString();
-
-		//rules only apply to string type
-		if ( varType == "bsl::string" || 
-	   	     varType == "std::string" ) {
-
-			clang::StringLiteral const * str = nodes.getNodeAs<clang::StringLiteral>("string"); 	
-
-			//if string literal is empty
-			if (str->getLength() == 0) {
-				a.report(varDecl, check_name, tag, message);
-
-				SourceRange range = SourceRange(varDecl->getSourceRange().getBegin(), varDecl->getInit()->getExprLoc());
-				
-				std::string replacement = a.get_source(range, true).str() + varDecl->getNameAsString(); 
-				
-				a.report(varDecl, check_name, tag,
-                     "Replace with %0", false, DiagnosticIDs::Note)
-                << replacement
-                << varDecl->getSourceRange();
-            	a.ReplaceText(a.get_full_range(varDecl->getSourceRange()), replacement);
-			}
-		}
+    std::string tag = "SI01";
+    std::string message = "strings should not be initialized with an empty string";
+    
+    SourceRange range; //range of source to be replaced
+    SourceLocation loc; //location of varDecl or initialiser
+    std::string varType; 
+    std::string replacement; //the new code 
+     
+    if (varInit == nullptr) { //if varibable declaration is matched
+        varType = varDecl->getInit()->getType().getAsString();
+        loc = varDecl->getSourceRange().getBegin();
+        range = varDecl->getSourceRange();
+        SourceRange declTypeRange = SourceRange(varDecl->getSourceRange().getBegin(), varDecl->getInit()->getExprLoc());
+        replacement = a.get_source(declTypeRange, true).str() + varDecl->getNameAsString(); 
     }
-	else {
-		std::string varType = varInit->getInit()->getType().getAsString();
+    else { //if initialiser is matched
+        varType = varInit->getInit()->getType().getAsString();
+        loc = varInit->getSourceLocation();
+        range = varInit->getSourceRange();
+        replacement = varInit->getMember()->getNameAsString() + "()"; 
+    }
 
-		if ( varType == "bsl::string" ||
-		     varType == "std::string" ) {
-
-			clang::StringLiteral const * str = nodes.getNodeAs<clang::StringLiteral>("string");
-			
-			if (str->getLength() == 0) {
-				a.report(varInit->getSourceLocation(), check_name, tag, message) << varInit->getSourceRange(); 
-			
-				SourceRange range = varInit->getSourceRange();
-					
-				std::string replacement = varInit->getMember()->getNameAsString() + "()"; 
-
-				a.report(varInit->getSourceLocation(), check_name, tag,
-                     "Replace with %0", false, DiagnosticIDs::Note)
-                << replacement
-                << range;
-            	a.ReplaceText(a.get_full_range(range), replacement);
-
-			}
-			
-		}
-	}
+    if ( varType == "bsl::string" ||
+         varType == "std::string" ) {
+                        
+        a.report(loc, check_name, tag, message) << range; 
+        a.report(loc, check_name, tag,
+            "Replace with %0", false, DiagnosticIDs::Note)
+            << replacement
+            << range;
+        a.ReplaceText(a.get_full_range(range), replacement);
+    }
 }
 
 
 void subscribe(Analyser& analyser, Visitor& visitor, PPObserver& observer) {
-	analyser.onTranslationUnitDone += report(analyser);	
+    analyser.onTranslationUnitDone += report(analyser); 
 }
 
 }  // close anonymous namespace
